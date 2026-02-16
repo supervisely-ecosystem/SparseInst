e407f495e3caff939ad5949c6a83810477f4f93b
vorozhkog <115161827+vorozhkog@users.noreply.github.com>
2026-02-16 21:21:05 +0000

    feat: Upgrade CUDA version to support latest NVIDIA GPU architecture

--- docker/publish.sh
+++ docker/publish.sh
@@ -1,2 +1,5 @@
-docker build -t supervisely/sparseinst:1.0.4 . && \
-docker push supervisely/sparseinst:1.0.4
+VER="1.1.0"
+TAG="supervisely/sparseinst:$VER"
+
+docker build -t $TAG . && \
+docker push $TAG
