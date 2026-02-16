9b3719b58d416fcb9f9af177e3d56386d633bcfc
vorozhkog <vorozhkog@icloud.com>
2026-02-16 18:18:24 +0000

    Update Dockerfile to install uvicorn using python3.12 and change image tag in publish script to dev-0.0.1

--- docker/publish.sh
+++ docker/publish.sh
@@ -1,2 +1,2 @@
-docker build -t supervisely/sparseinst:1.0.9 . && \
-docker push supervisely/sparseinst:1.0.9
+docker build -t supervisely/sparseinst:dev-0.0.1 . && \
+docker push supervisely/sparseinst:dev-0.0.1
