512db323fd21a332dbf3da8be350a0842476c2e5
vorozhkog <vorozhkog@icloud.com>
2026-02-16 17:47:33 +0000

    Update Docker image tag to 1.0.9 in publish script

--- docker/publish.sh
+++ docker/publish.sh
@@ -1,2 +1,2 @@
-docker build -t supervisely/sparseinst:1.0.8 . && \
-docker push supervisely/sparseinst:1.0.8
+docker build -t supervisely/sparseinst:1.0.9 . && \
+docker push supervisely/sparseinst:1.0.9
