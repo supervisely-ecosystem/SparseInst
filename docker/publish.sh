50a64e36c639a9c422e3731dc81b9fdf803e42ee
vorozhkog <vorozhkog@icloud.com>
2026-02-16 18:45:51 +0000

    Update publish script to use docker image tag dev-0.0.2

--- docker/publish.sh
+++ docker/publish.sh
@@ -1,2 +1,2 @@
-docker build -t supervisely/sparseinst:dev-0.0.1 . && \
-docker push supervisely/sparseinst:dev-0.0.1
+docker build -t supervisely/sparseinst:dev-0.0.2 . && \
+docker push supervisely/sparseinst:dev-0.0.2
